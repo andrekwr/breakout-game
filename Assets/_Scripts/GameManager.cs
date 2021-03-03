3a52dcc6b2e344245e0922e9a2a48e557da879b8
Andre-Weber <andreew@al.insper.edu.br>
2021-03-03 01:24:35 -0300

    unity game finished

---
+++ Assets/_Scripts/GameManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager
+{
+    private static GameManager _instance;
+
+
+    public static GameManager GetInstance()
+    {
+        if(_instance == null)
+        {
+            _instance = new GameManager();
+        }
+
+        return _instance;
+    }
+
+    public enum GameState { MENU, GAME, PAUSE, ENDGAME };
+
+    public GameState gameState { get; private set; }
+    public int vidas;
+    public int pontos;
+
+    private GameManager()
+    {
+        vidas = 3;
+        pontos = 0;
+        gameState = GameState.MENU;
+
+    }
+
+    
+    public delegate void ChangeStateDelegate();
+    public static ChangeStateDelegate changeStateDelegate;
+
+    public void ChangeState(GameState nextState)
+    {
+        if (nextState == GameState.GAME) Reset();
+        gameState = nextState;
+        changeStateDelegate();
+    }
+    private void Reset()
+        {
+        vidas = 3;
+        pontos = 0;
+        }
+}
---
+++ Assets/_Scripts/UI_FimDeJogo.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI_FimDeJogo : MonoBehaviour
+{
+   public Text message;
+
+    GameManager gm;
+   private void OnEnable()
+   {
+       Debug.Log("entrou debug");
+       gm = GameManager.GetInstance();
+
+       if(gm.vidas > 0)
+       {
+           message.text = "VOCÊ GANHOU!";
+       }
+       else
+       {
+           message.text = "VOCÊ PERDEU!";
+       }
+   }
+
+   public void Voltar()
+    {
+        gm.ChangeState(GameManager.GameState.GAME);
+    }
+}
